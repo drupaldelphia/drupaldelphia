d38940e346950f55dcb16a2950ca36fc3d071bed
Ted Slesinski <tslesinski@navigationarts.com>
2013-04-21 12:37:47 -0400

    Style updates

--- sites/all/themes/drupaldelphia/js/script.js
+++ sites/all/themes/drupaldelphia/js/script.js
@@ -10,6 +10,9 @@
       $('.mobile-handle').click(function() {
         $('body').toggleClass('slide');
       });
+      $('select')
+        .wrap('<div class="input-wrap" />')
+        .after('<span class="select" />');
     }
   };
 })(jQuery);
