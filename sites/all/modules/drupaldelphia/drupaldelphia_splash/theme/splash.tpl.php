7556765ababfd2ed80160d4ec1faca4e66f6fdde
ted <ted@slesinski.org>
2013-06-12 08:52:02 -0400

    Put splash page in code, updated drupaldelphia modules

---
+++ sites/all/modules/drupaldelphia/drupaldelphia_splash/theme/splash.tpl.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @file
+ * Available variables:
+ */
+
+?>
+
+<div class="splash-area">
+  <h2><?php print t('Philadelphia'); ?></h2>
+
+  <h3 class="ribbon">
+    <?php print t('October 1st, 2013'); ?>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+  </h3>
+
+  <div>
+    <a class="btn" href="/user/register"><?php print t('Get a Ticket'); ?></a>
+  </div>
+</div>
