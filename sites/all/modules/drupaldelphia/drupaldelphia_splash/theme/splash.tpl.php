31d7f809612f3218596555e42ec5dbb53a5a8404
ted <ted@slesinski.org>
2013-06-13 00:30:56 -0400

    Added clearfixes to splash markup, tweaked mobile menu

--- sites/all/modules/drupaldelphia/drupaldelphia_splash/theme/splash.tpl.php
+++ sites/all/modules/drupaldelphia/drupaldelphia_splash/theme/splash.tpl.php
@@ -1,24 +1,24 @@
 <?php
-
 /**
  * @file
  * Available variables:
  */
-
 ?>
 
 <div class="splash-area">
-  <h2><?php print t('Philadelphia'); ?></h2>
-
-  <h3 class="ribbon">
-    <?php print t('October 1st, 2013'); ?>
-    <i></i>
-    <i></i>
-    <i></i>
-    <i></i>
-  </h3>
-
-  <div>
+  <div class="clearfix">
+    <h2><?php print t('Philadelphia'); ?></h2>
+  </div>
+  <div class="clearfix">
+    <h3 class="ribbon">
+      <?php print t('October 1st, 2013'); ?>
+      <i></i>
+      <i></i>
+      <i></i>
+      <i></i>
+    </h3>
+  </div>
+  <div class="clearfix">
     <?php print l(t('Buy a Ticket'), 'content/drupaldelphia-2013', array('attributes' => array('class' => array('btn')))); ?>
     <?php print l(t('Submit a Session'), 'node/add/session', array('attributes' => array('class' => array('btn')))); ?>
   </div>
