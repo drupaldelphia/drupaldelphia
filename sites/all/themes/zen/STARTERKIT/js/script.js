25fbeef051644d20a709c17063d8869e57c1348a
ted <tslesinski@navigationarts.com>
2013-08-16 10:17:46 -0400

    Revert "Updated Zen"
    This reverts commit 7dd7494bc26ff3c075f06107b7731c66206bf71e.

--- sites/all/themes/zen/STARTERKIT/js/script.js
+++ sites/all/themes/zen/STARTERKIT/js/script.js
@@ -8,19 +8,12 @@
 
 // JavaScript should be made compatible with libraries other than jQuery by
 // wrapping it with an "anonymous closure". See:
-// - https://drupal.org/node/1446420
+// - http://drupal.org/node/1446420
 // - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
 (function ($, Drupal, window, document, undefined) {
 
 
-// To understand behaviors, see https://drupal.org/node/756722#behaviors
-Drupal.behaviors.my_custom_behavior = {
-  attach: function(context, settings) {
-
-    // Place your code here.
-
-  }
-};
+// Place your code here.
 
 
 })(jQuery, Drupal, this, this.document);
